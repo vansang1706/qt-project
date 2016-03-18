761aec64acd6939496e974a19ea5bc12aca8d118
vansang1706 <vansang1706@gmail.com>
2016-03-18 15:55:58 +0700

    Update

--- QT/QT.Services/ISanPhamService.cs
+++ QT/QT.Services/ISanPhamService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using QT.Models;
+﻿using QT.Models;
 using QT.Service.Pattern;
 
 namespace QT.Services
