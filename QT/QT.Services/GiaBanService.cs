44c16e59238fd07986ffeb3c4e031099749b6aa2
vansang1706 <vansang1706@gmail.com>
2016-03-23 23:29:30 +0700

    update

--- QT/QT.Services/GiaBanService.cs
+++ QT/QT.Services/GiaBanService.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QT.Models;
+using QT.Repository.Pattern.UnitOfWork;
+using QT.Service.Pattern;
 
 namespace QT.Services
 {
-    class GiaBanService
+    
+    public class GiaBanService:Service<GiaBan>,IGiaBanService
     {
+        private readonly IUnitOfWorkAsync _unitOfWork;
+
+        public GiaBanService(IUnitOfWorkAsync unitOfWork) : base(unitOfWork.RepositoryAsync<GiaBan>())
+        {
+            _unitOfWork = unitOfWork;
+        }
     }
 }
