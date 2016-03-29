1c6246d57b49b9f4a1e9f7336e41706f4fbb5770
vansang1706 <vansang1706@gmail.com>
2016-03-30 00:22:22 +0700

    home

--- QT/QT.App/HoaDonForm.Designer.cs
+++ QT/QT.App/HoaDonForm.Designer.cs
@@ -34,14 +34,25 @@
             this.btCapNhatHoaDon = new System.Windows.Forms.Button();
             this.btThemHoaDon = new System.Windows.Forms.Button();
             this.dgvHoaDon = new System.Windows.Forms.DataGridView();
-            this.tabThemSuaHoaDon = new System.Windows.Forms.TabPage();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.khachHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tongTien = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ngayLap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabThemSuaHoaDon = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbKhachHang = new System.Windows.Forms.ComboBox();
+            this.dgvThemSuaHoaDon = new System.Windows.Forms.DataGridView();
+            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sanPham = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.soLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.giaBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.thanhTien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.delete = new System.Windows.Forms.DataGridViewButtonColumn();
             this.tabControl1.SuspendLayout();
             this.tabQuanLyHoaDon.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
+            this.tabThemSuaHoaDon.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThemSuaHoaDon)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -51,7 +62,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(676, 276);
+            this.tabControl1.Size = new System.Drawing.Size(722, 276);
             this.tabControl1.TabIndex = 0;
             // 
             // tabQuanLyHoaDon
@@ -94,6 +105,7 @@
             this.btThemHoaDon.TabIndex = 1;
             this.btThemHoaDon.Text = "Thêm Hóa Đơn";
             this.btThemHoaDon.UseVisualStyleBackColor = true;
+            this.btThemHoaDon.Click += new System.EventHandler(this.btThemHoaDon_Click);
             // 
             // dgvHoaDon
             // 
@@ -107,16 +119,6 @@
             this.dgvHoaDon.Name = "dgvHoaDon";
             this.dgvHoaDon.Size = new System.Drawing.Size(656, 150);
             this.dgvHoaDon.TabIndex = 0;
-            // 
-            // tabThemSuaHoaDon
-            // 
-            this.tabThemSuaHoaDon.Location = new System.Drawing.Point(4, 22);
-            this.tabThemSuaHoaDon.Name = "tabThemSuaHoaDon";
-            this.tabThemSuaHoaDon.Padding = new System.Windows.Forms.Padding(3);
-            this.tabThemSuaHoaDon.Size = new System.Drawing.Size(668, 250);
-            this.tabThemSuaHoaDon.TabIndex = 1;
-            this.tabThemSuaHoaDon.Text = "Thêm Hóa Đơn";
-            this.tabThemSuaHoaDon.UseVisualStyleBackColor = true;
             // 
             // id
             // 
@@ -148,6 +150,92 @@
             this.ngayLap.Name = "ngayLap";
             this.ngayLap.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
+            // tabThemSuaHoaDon
+            // 
+            this.tabThemSuaHoaDon.Controls.Add(this.dgvThemSuaHoaDon);
+            this.tabThemSuaHoaDon.Controls.Add(this.cbKhachHang);
+            this.tabThemSuaHoaDon.Controls.Add(this.label1);
+            this.tabThemSuaHoaDon.Location = new System.Drawing.Point(4, 22);
+            this.tabThemSuaHoaDon.Name = "tabThemSuaHoaDon";
+            this.tabThemSuaHoaDon.Padding = new System.Windows.Forms.Padding(3);
+            this.tabThemSuaHoaDon.Size = new System.Drawing.Size(714, 250);
+            this.tabThemSuaHoaDon.TabIndex = 1;
+            this.tabThemSuaHoaDon.Text = "Thêm Hóa Đơn";
+            this.tabThemSuaHoaDon.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Khách Hàng";
+            // 
+            // cbKhachHang
+            // 
+            this.cbKhachHang.FormattingEnabled = true;
+            this.cbKhachHang.Location = new System.Drawing.Point(117, 18);
+            this.cbKhachHang.Name = "cbKhachHang";
+            this.cbKhachHang.Size = new System.Drawing.Size(121, 21);
+            this.cbKhachHang.TabIndex = 1;
+            // 
+            // dgvThemSuaHoaDon
+            // 
+            this.dgvThemSuaHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThemSuaHoaDon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stt,
+            this.sanPham,
+            this.soLuong,
+            this.giaBan,
+            this.thanhTien,
+            this.delete});
+            this.dgvThemSuaHoaDon.Location = new System.Drawing.Point(25, 71);
+            this.dgvThemSuaHoaDon.Name = "dgvThemSuaHoaDon";
+            this.dgvThemSuaHoaDon.Size = new System.Drawing.Size(662, 150);
+            this.dgvThemSuaHoaDon.TabIndex = 2;
+            this.dgvThemSuaHoaDon.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThemSuaHoaDon_CellContentClick);
+            this.dgvThemSuaHoaDon.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgvThemSuaHoaDon_RowsAdded);
+            this.dgvThemSuaHoaDon.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.dgvThemSuaHoaDon_RowsRemoved);
+            // 
+            // stt
+            // 
+            this.stt.HeaderText = "STT";
+            this.stt.Name = "stt";
+            this.stt.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.stt.Width = 50;
+            // 
+            // sanPham
+            // 
+            this.sanPham.HeaderText = "Sản Phẩm";
+            this.sanPham.Name = "sanPham";
+            this.sanPham.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.sanPham.Width = 150;
+            // 
+            // soLuong
+            // 
+            this.soLuong.HeaderText = "Số Lượng";
+            this.soLuong.Name = "soLuong";
+            this.soLuong.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // giaBan
+            // 
+            this.giaBan.HeaderText = "Giá Bán";
+            this.giaBan.Name = "giaBan";
+            this.giaBan.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // thanhTien
+            // 
+            this.thanhTien.HeaderText = "Thành Tiền";
+            this.thanhTien.Name = "thanhTien";
+            this.thanhTien.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // delete
+            // 
+            this.delete.HeaderText = "";
+            this.delete.Name = "delete";
+            this.delete.Text = "Delete";
+            // 
             // HoaDonForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -162,6 +250,9 @@
             this.tabControl1.ResumeLayout(false);
             this.tabQuanLyHoaDon.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
+            this.tabThemSuaHoaDon.ResumeLayout(false);
+            this.tabThemSuaHoaDon.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThemSuaHoaDon)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -179,5 +270,14 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn khachHang;
         private System.Windows.Forms.DataGridViewTextBoxColumn tongTien;
         private System.Windows.Forms.DataGridViewTextBoxColumn ngayLap;
+        private System.Windows.Forms.DataGridView dgvThemSuaHoaDon;
+        private System.Windows.Forms.ComboBox cbKhachHang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
+        private System.Windows.Forms.DataGridViewComboBoxColumn sanPham;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soLuong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn giaBan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn thanhTien;
+        private System.Windows.Forms.DataGridViewButtonColumn delete;
     }
 }
--- QT/QT.App/HoaDonForm.cs
+++ QT/QT.App/HoaDonForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using Microsoft.Practices.Unity;
+using QT.Globalization;
 using QT.IoC.Unity;
 using QT.Services;
 
@@ -9,6 +10,8 @@ namespace QT.App
     public partial class HoaDonForm : Form
     {
         private readonly IHoaDonService _hoaDonService;
+        private readonly IKhachHangService _khachHangService;
+        private readonly ISanPhamService _sanPhamService;
         //private SanPham _sanPhamToUpdate;
         private static HoaDonForm _instance;
 
@@ -25,6 +28,8 @@ namespace QT.App
         {
             InitializeComponent();
             _hoaDonService = UnityConfig.GetContainer().Resolve<IHoaDonService>();
+            _khachHangService = UnityConfig.GetContainer().Resolve<IKhachHangService>();
+            _sanPhamService = UnityConfig.GetContainer().Resolve<ISanPhamService>();
         }
 
         private void HoaDonForm_Load(object sender, EventArgs e)
@@ -38,6 +43,49 @@ namespace QT.App
         {
             var hoaDons = _hoaDonService.GetAllHoaDonToDataGridView();
             dgvHoaDon.DataSource = hoaDons;
+        }
+
+        private void btThemHoaDon_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(tabThemSuaHoaDon))
+            {
+                tabControl1.TabPages.Add(tabThemSuaHoaDon);
+                tabThemSuaHoaDon.Text = QT_Messages.ThemGiaBanText;
+                tabControl1.SelectedTab = tabThemSuaHoaDon;
+
+                var khachHangs = _khachHangService.GetAllKhachHang();
+                cbKhachHang.DataSource = khachHangs;
+                cbKhachHang.DisplayMember = "HoTen";
+                cbKhachHang.ValueMember = "Id";
+
+                sanPham.DataSource = _sanPhamService.GetAllSanPham();
+                sanPham.DisplayMember = "TenSanPham";
+                sanPham.ValueMember = "Id";
+
+                delete.Text = "Delete";
+                delete.UseColumnTextForButtonValue = true;
+            }
+        }
+
+        private void dgvThemSuaHoaDon_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            if (dgvThemSuaHoaDon.Columns.Contains("stt"))
+            {
+                foreach (DataGridViewRow r in dgvThemSuaHoaDon.Rows)
+                {
+                    r.Cells["stt"].Value = r.Index + 1;
+                }
+            }
+        }
+
+        private void dgvThemSuaHoaDon_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+
+        }
+
+        private void dgvThemSuaHoaDon_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
